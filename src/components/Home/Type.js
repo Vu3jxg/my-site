8c7a3f06bf8f2adb67e693c5380a4db30a648fab
pal.gaurav007 <pal.gaurav007@gmail.com>
2024-09-17 16:54:24 +0530

    initial commit

---
+++ src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Software Developer",
+          "IoT Interoperability",
+          "Data Science Enthusiastic",
+          "Open Source Contributor",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
