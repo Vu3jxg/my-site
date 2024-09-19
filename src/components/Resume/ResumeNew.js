9c883cc06f43e837a97966c832257e76ad311662
pal.gaurav007 <pal.gaurav007@gmail.com>
2024-09-19 10:05:16 +0530

    updated resume and navbar changes

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/GAURAV-ATS-Resume.pdf";
+import pdf from "../../Assets/../Assets/Modern_Simple_ATS_Friendly_LateX_CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
